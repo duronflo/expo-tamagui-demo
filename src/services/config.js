a79032f90b28b655a4c26bb03c88b4f557afd3e8
duronflo <flo@durons.net>
2025-08-20 22:06:38 +0200

    delete files

--- src/services/config.js
+++
@@ -1,8 +0,0 @@
-export default firebaseConfig = {
-    apiKey: "AIzaSyAsCAFDP-fekQMTUwbwaVfIcA19NUB0CJw",
-    authDomain: "mymonji-9bbac.firebaseapp.com",
-    projectId: "mymonji-9bbac",
-    storageBucket: "mymonji-9bbac.appspot.com",
-    messagingSenderId: "754591635099",
-    appId: "1:754591635099:web:3c8cc8f781d1789d14583e"
-};
